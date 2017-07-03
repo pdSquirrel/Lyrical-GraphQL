4109799d67f46eda581dc3749d2bb008a7eeb643
Roger Nordqvist <progdev.squirrel@gmail.com>
2017-07-03 23:35:24 +0200

    added SongCreate

--- client/index.js
+++ client/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import App from './components/App';
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 /* by default, looks at /graphql */
 const client = new ApolloClient({});
@@ -11,7 +14,12 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="song/new" component={SongCreate} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
